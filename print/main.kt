05135920794c77ebffbf50c2afd5429ee48fc861
Joey <43940776+adoboligh1@users.noreply.github.com>
2022-10-27 19:10:15 +0000

    Create main.kt
    hope i  don suck lol

---
+++ print/main.kt
@@ -0,0 +1,26 @@
+/*created by Albert Adoboli */
+
+fun main() {
+  
+  println("Hello World") //Prints hello world to the console.
+  
+  
+  val name1 = "kofi" //creates a value of kofi.
+  val name2 = "ama" ////creates a value of ama.
+  val numbers = 5 // creates a value 5.
+  
+  
+  println(name1) //prints kofi
+  println(name2) //prints ama
+  println(numbers) //ptints 5.
+  
+  
+  var name3 = "joey" //creates a variable named joey.
+  name3 = "joey3" //changes the variable name3 containing joey to joey3 you cant do this with VAL which can not be changed.
+  
+  
+  println(name3)
+
+
+
+}
